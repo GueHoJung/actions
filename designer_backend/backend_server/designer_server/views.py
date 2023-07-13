5a9ee34c48b953606583574302725fe8b2ac1a10
GueHoJung <peace_and_love@kakao.com>
2023-07-13 14:51:42 +0900

    config: 정상화 DRF 및 Swagger 로컬 세팅 정상화
    정상화 완료, DRF 및 Swagger 로컬 세팅 완료

--- designer_backend/backend_server/designer_server/views.py
+++ designer_backend/backend_server/designer_server/views.py
@@ -1,6 +1,11 @@
+import json
+from django.shortcuts import render
+from django.core import serializers
+from django.http import JsonResponse
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.shortcuts import render
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.views import APIView
 
@@ -14,17 +19,24 @@ class ItemViewSet(viewsets.ModelViewSet):
     queryset = DesignerTest.objects.all()
     serializer_class = ItemSerializer
 
+
 # api_view decorator django rest framework 방식
 @api_view(["GET"])
 def designer_test(request):
     queryset = DesignerTest.objects.all()
+    print("queryset : "+str(queryset))
     serializer = ItemSerializer(queryset, many=True)
+    # print("serializer.data : "+str(serializer.data))
+    # queryset_json = json.loads(serializers.serialize('json', queryset, ensure_ascii=False))
+    # print("queryset_json : "+str(queryset_json))
     return Response(serializer.data)
+    # return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+    # return JsonResponse({"data": queryset_json}, status=status.HTTP_200_OK)
 
 # APIView django rest framework 방식
 class DesignerTestAPI(APIView):
     def get(self, request):
         queryset = DesignerTest.objects.all()
         print(queryset)
-        serializer = ItemSerializer(queryset)
+        serializer = ItemSerializer(queryset, many=True)
         return Response(serializer.data)
